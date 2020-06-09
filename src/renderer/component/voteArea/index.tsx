81cfd9e806150997bfdb83232c311b13a3a43a25
wuyunfeng <wuyunfeng@kuaishou.com>
2020-06-09 20:19:44 +0800

    feat(cylindergeometry): 圆柱绘制存在问题

--- src/renderer/component/voteArea/index.tsx
+++ src/renderer/component/voteArea/index.tsx
@@ -10,35 +10,34 @@ var geometry, material, mesh;
 
 function init() {
 
-	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 1;
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
+  camera.position.z = 1;
 
-	scene = new THREE.Scene();
+  scene = new THREE.Scene();
 
-	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-	material = new THREE.MeshNormalMaterial();
+  geometry = new THREE.CylinderGeometry(0.5, 0.5, 0.5);
+  material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
 
-	mesh = new THREE.Mesh( geometry, material );
-	scene.add( mesh );
-
-	renderer = new THREE.WebGLRenderer( { antialias: true } );
-	renderer.setSize( window.innerWidth, window.innerHeight );
-	document.getElementById('gl').appendChild( renderer.domElement );
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.getElementById('gl').appendChild(renderer.domElement);
 
 }
 
 function animate() {
 
-	requestAnimationFrame( animate );
+  /* requestAnimationFrame(animate);
 
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.02;
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.02; */
 
-	renderer.render( scene, camera );
+  renderer.render(scene, camera);
 
 }
 
-interface IVoteArea {}
+interface IVoteArea { }
 
 interface IVote {
   name: string;
@@ -60,13 +59,13 @@ const MAXCOUNT = 5;
  */
 export default function VoteArea(props: Readonly<IVoteArea>) {
   const [voteList, setVoteList] = useState<IVote[]>([]);
-  
+
   const onVoteAdd = () => {
     if (voteList.length === MAXCOUNT) {
       alert('最多5个')
       return;
     }
-    setVoteList([ {...defaultValue}, ...voteList])
+    setVoteList([{ ...defaultValue }, ...voteList])
   }
 
   const onValueChange = (name: string, index: number) => {
@@ -89,18 +88,18 @@ export default function VoteArea(props: Readonly<IVoteArea>) {
       <button onClick={onVoteAdd}>新增投票</button>
       {
         voteList.map((item: IVote, index: number) => {
-          return item.editable?
-          <input className="vote-item" onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
-            if (e.keyCode === 13) {
+          return item.editable ?
+            <input className="vote-item" onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+              if (e.keyCode === 13) {
+                onEditableChange(index);
+              }
+            }} key={index} type="text" onChange={(e) => {
+              onValueChange(e.target.value, index)
+            }} value={item.name} />
+            :
+            <span onDoubleClick={() => {
               onEditableChange(index);
-            }
-          }} key={index} type="text" onChange={(e) => {
-            onValueChange(e.target.value, index)
-          }} value={item.name} />
-          :
-          <span onDoubleClick={() => {
-            onEditableChange(index);
-          }} className="vote-item" key={index}>{item.name}</span>
+            }} className="vote-item" key={index}>{item.name}</span>
         })
       }
     </div>
